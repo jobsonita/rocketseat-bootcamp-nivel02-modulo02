8fc9eb18af0d05b49390bcb3c10c9f29eaf90562
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2020-04-25 05:43:33 -0300

    sessão04-aula03 - Upload de imagens - Servindo arquivos estáticos

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import 'reflect-metadata'
 import express from 'express'
 
 import './database'
+
+import { tmpDir } from './config/upload'
+
 import routes from './routes'
 
 const app = express()
@@ -9,6 +12,8 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+app.use('/files', express.static(tmpDir))
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!')
